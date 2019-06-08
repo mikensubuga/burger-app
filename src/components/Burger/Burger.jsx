81f7b3fc28e3cfc47de2a767394b8789f7091d09
King-Mike <nsubugamike021@gmail.com>
2019-06-08 18:57:43 +0300

    rendered ing dynamically

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -2,12 +2,20 @@ import React from "react";
 
 import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
+
 const burger = props => {
+  let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+    return [...Array(props.ingredients[igKey])].map((_, i) => {
+      return <BurgerIngredient key={igKey + i} type={igKey} />;
+    });
+  });
+
+  console.log("props:" + transformedIngredients);
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
 import Burger from "../../components/Burger/Burger";
 class BurgerBuilder extends Component {
-  state = {};
+  state = {
+    ingredients: {
+      salad: 1,
+      cheese: 2,
+      meat: 2,
+      bacon: 1
+    }
+  };
   render() {
     return (
       <div>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </div>
     );
