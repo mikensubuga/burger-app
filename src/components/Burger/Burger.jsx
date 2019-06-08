5673f279445771617fa0ce94692b12ab553892c1
King-Mike <nsubugamike021@gmail.com>
2019-06-08 19:10:07 +0300

    ing if empty

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -4,13 +4,19 @@ import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = props => {
-  let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      return <BurgerIngredient key={igKey + i} type={igKey} />;
-    });
-  });
-
-  console.log("props:" + transformedIngredients);
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
+  console.log(transformedIngredients);
 
   return (
     <div className={classes.Burger}>
--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -3,10 +3,10 @@ import Burger from "../../components/Burger/Burger";
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
-      salad: 1,
-      cheese: 2,
-      meat: 2,
-      bacon: 1
+      salad: 0,
+      cheese: 0,
+      meat: 0,
+      bacon: 0
     }
   };
   render() {
