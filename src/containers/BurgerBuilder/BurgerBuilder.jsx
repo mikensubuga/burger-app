33675b28e3f3ceaaa1bb4cc8c888194cd7aa999e
King-Mike <nsubugamike021@gmail.com>
2019-06-08 20:32:05 +0300

    added state to build controls(more btn)

--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from "react";
 import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
+
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7
+};
 class BurgerBuilder extends Component {
   state = {
     ingredients: {
@@ -8,16 +15,35 @@ class BurgerBuilder extends Component {
       cheese: 0,
       meat: 0,
       bacon: 0
-    }
+    },
+    totalPrice: 4
   };
   render() {
     return (
       <div>
         <Burger ingredients={this.state.ingredients} />
-        <BuildControls />
+        <BuildControls ingredientAdded={this.addIngredientHandler} />
       </div>
     );
   }
+
+  addIngredientHandler = type => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount + 1;
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+    updatedIngredients[type] = updatedCount;
+    const priceAdditon = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAdditon;
+    this.setState({
+      totalPrice: newPrice,
+      ingredients: updatedIngredients
+    });
+  };
+
+  removeIngredientHandler = type => {};
 }
 
 export default BurgerBuilder;
