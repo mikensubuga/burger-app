00d6deb7e1671b8bff271c9f889c687d09d9a2dc
King-Mike <nsubugamike021@gmail.com>
2019-06-08 21:34:13 +0300

    displaying and updating burger price

--- src/components/Burger/BuildControls/BuildControls.jsx
+++ src/components/Burger/BuildControls/BuildControls.jsx
@@ -10,6 +10,9 @@ const controls = [
 ];
 const buildControls = props => (
   <div className={classes.BuildControls}>
+    <p>
+      Current price:<strong>{props.price.toFixed(2)}</strong>
+    </p>
     {controls.map(ctrl => (
       <BuildControl
         key={ctrl.label}
