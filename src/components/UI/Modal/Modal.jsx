18cf417962005f8f2bccb920516350913ed9d413
King-Mike <nsubugamike021@gmail.com>
2019-06-10 21:30:47 +0300

    added summary modal

---
+++ src/components/UI/Modal/Modal.jsx
@@ -0,0 +1,4 @@
+import React, { Component } from "react";
+import classes from "./Modal.module.css";
+const modal = props => <div className={classes.Modal}>{props.children}</div>;
+export default modal;
--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
-
+import Modal from "../../components/UI/Modal/Modal";
+import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
 const INGREDIENT_PRICES = {
   salad: 0.5,
   cheese: 0.4,
@@ -28,6 +29,9 @@ class BurgerBuilder extends Component {
     }
     return (
       <div>
+        <Modal>
+          <OrderSummary ingredients={this.state.ingredients} />
+        </Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
           ingredientAdded={this.addIngredientHandler}
