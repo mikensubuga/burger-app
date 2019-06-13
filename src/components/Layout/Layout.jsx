a60551e7d1e13da4efeadf405e48db8d01fc7d1c
King-Mike <nsubugamike021@gmail.com>
2019-06-13 22:19:47 +0300

    added toolbar

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import * as classes from "./Layout.module.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+
 const layout = props => (
   <div>
-    <div>Toolbar, SideDrawer, BackDrop</div>
+    {/* <div>Toolbar, SideDrawer, BackDrop</div> */}
+    <Toolbar />
     <main className={classes.Content}>{props.children}</main>
   </div>
 );
---
+++ src/components/Navigation/Toolbar/Toolbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import classes from "./Toolbar.module.css";
+const toolbar = props => (
+  <header className={classes.Toolbar}>
+    <div>MENU</div>
+    <div>Logo</div>
+    <nav>...</nav>
+  </header>
+);
+
+export default toolbar;
