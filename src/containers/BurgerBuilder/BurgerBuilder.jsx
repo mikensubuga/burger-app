d86bc09bf6b08a936bbcd72c505587e709bcd7f9
King-Mike <nsubugamike021@gmail.com>
2019-06-17 17:40:01 +0300

    post request to server

--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -3,6 +3,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-order";
 const INGREDIENT_PRICES = {
   salad: 0.5,
   cheese: 0.4,
@@ -60,7 +61,22 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinuehander = () => {
-    alert("You Continue");
+    //alert("You Continue");
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: "Jane Doe",
+        address: {
+          country: "Uganda"
+        },
+        email: "nmike@cis.mak.ac.ug"
+      }
+    };
+    axios
+      .post("/orders.json", order)
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
   };
   updatePurchaseState(ingredients) {
     // const ingredients = {
