8e55d6d993f754b0f4e8d5b99a94b66947626759
King-Mike <nsubugamike021@gmail.com>
2019-06-17 21:31:09 +0300

    added spinner while making request

--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -4,6 +4,7 @@ import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
 import axios from "../../axios-order";
+import Spinner from "../../components/UI/Spinner/Spinner";
 const INGREDIENT_PRICES = {
   salad: 0.5,
   cheese: 0.4,
@@ -20,7 +21,8 @@ class BurgerBuilder extends Component {
     },
     totalPrice: 4,
     purchasable: false,
-    purchasing: false
+    purchasing: false,
+    loading: false
   };
   render() {
     const disabledInfo = {
@@ -29,18 +31,25 @@ class BurgerBuilder extends Component {
     for (let key in disabledInfo) {
       disabledInfo[key] = disabledInfo[key] <= 0;
     }
+    let orderSummary = (
+      <OrderSummary
+        ingredients={this.state.ingredients}
+        continue={this.purchaseContinuehander}
+        cancel={this.purchaseCancelhandler}
+        totalPrice={this.state.totalPrice}
+      />
+    );
+
+    if (this.state.loading) {
+      orderSummary = <Spinner />;
+    }
     return (
       <div>
         <Modal
           show={this.state.purchasing}
           modalClosed={this.purchaseCancelhandler}
         >
-          <OrderSummary
-            ingredients={this.state.ingredients}
-            continue={this.purchaseContinuehander}
-            cancel={this.purchaseCancelhandler}
-            totalPrice={this.state.totalPrice}
-          />
+          {orderSummary}
         </Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
@@ -62,6 +71,7 @@ class BurgerBuilder extends Component {
 
   purchaseContinuehander = () => {
     //alert("You Continue");
+    this.setState({ loading: true });
     const order = {
       ingredients: this.state.ingredients,
       price: this.state.totalPrice,
@@ -75,8 +85,14 @@ class BurgerBuilder extends Component {
     };
     axios
       .post("/orders.json", order)
-      .then(response => console.log(response))
-      .catch(error => console.log(error));
+      .then(response => {
+        this.setState({ loading: false, purchasing: false });
+        console.log(response);
+      })
+      .catch(error => {
+        this.setState({ loading: false, purchasing: false });
+        console.log(error);
+      });
   };
   updatePurchaseState(ingredients) {
     // const ingredients = {
