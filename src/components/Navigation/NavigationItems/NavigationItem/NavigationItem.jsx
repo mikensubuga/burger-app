5e495d66c821d7e8dddbec34f4ff10fa057f792e
King-Mike <nsubugamike021@gmail.com>
2019-06-22 22:16:26 +0300

    implement nav links

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.jsx
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import classes from "./NavigationItem.module.css";
+import { NavLink } from "react-router-dom";
+
 const navigationItem = props => (
   <li className={classes.NavigationItem}>
-    <a href={props.link} className={props.active ? classes.active : null}>
+    {/* <a href={props.link} className={props.active ? classes.active : null}>
       {props.children}
-    </a>
+    </a> */}
+    <NavLink
+      exact={props.exact}
+      to={props.link}
+      activeClassName={classes.active}
+    >
+      {props.children}
+    </NavLink>
   </li>
 );
 
--- src/components/Navigation/NavigationItems/NavigationItems.jsx
+++ src/components/Navigation/NavigationItems/NavigationItems.jsx
@@ -3,10 +3,10 @@ import classes from "./NavigationItems.module.css";
 import NavigationItem from "./NavigationItem/NavigationItem";
 const navigationItems = () => (
   <ul className={classes.NavigationItems}>
-    <NavigationItem link="/" active>
+    <NavigationItem link="/" exact>
       Burger Builder
     </NavigationItem>
-    <NavigationItem link="/">Checkout</NavigationItem>
+    <NavigationItem link="/orders">Orders</NavigationItem>
   </ul>
 );
 
