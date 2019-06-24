8acab6956ca2b5e3ba8c13875cd298392b162ac9
King-Mike <nsubugamike021@gmail.com>
2019-06-24 15:44:47 +0300

    fixed validation bug

--- src/containers/Checkout/ContactData/ContactData.jsx
+++ src/containers/Checkout/ContactData/ContactData.jsx
@@ -102,13 +102,13 @@ class ContactData extends Component {
   checkValidity(value, rules) {
     let isValid = false;
     if (rules.required) {
-      isValid = value.trim() !== "";
+      isValid = value.trim() !== "" && isValid;
     }
     if (rules.minLength) {
-      isValid = value.length >= rules.minLength;
+      isValid = value.length >= rules.minLength && isValid;
     }
     if (rules.maxLength) {
-      isValid = value.length >= rules.maxLength;
+      isValid = value.length >= rules.maxLength && isValid;
     }
     return isValid;
   }
