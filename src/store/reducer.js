923b81a33a6f4062d074986fc4049b92597cee74
King-Mike <nsubugamike021@gmail.com>
2019-06-28 13:17:25 +0300

    finishing reducer 4 ing

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -11,6 +11,29 @@ const initialState = {
   totalPrice: 4
 };
 
-const reducer = (state = initialState, action) => {};
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.ADD_INGREDIENT:
+      return {
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+        }
+      };
+    case actionTypes.REMOVE_INGREDIENT:
+      return {
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          [action.ingredientName]: state.ingredients[action.ingredientName] - 1
+        }
+      };
+
+    default:
+      return state;
+      break;
+  }
+};
 
 export default reducer;
