3ecd8a19800b8a36103b454e7bc97a28bc73626b
King-Mike <nsubugamike021@gmail.com>
2019-07-02 13:29:18 +0300

    resetting the initial px after purchase

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -44,7 +44,8 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         ingredients: action.ingredients,
-        error: false
+        error: false,
+        totalPrice: 4.0
       };
     case actionTypes.FETCH_INGREDIENTS_FAILED:
       return {
