1b3c4ca6fa2a53ae6d30e5d9db202934a253d0fa
King-Mike <nsubugamike021@gmail.com>
2019-07-03 10:41:50 +0300

    added actions

--- src/containers/Auth/Auth.jsx
+++ src/containers/Auth/Auth.jsx
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Input from "../../components/UI/Input/Input";
 import Button from "../../components/UI/Button/Button";
 import classes from "./Auth.module.css";
+import * as actions from "../../store/actions/index";
+import { connect } from "react-redux";
 class Auth extends Component {
   state = {
     controls: {
@@ -80,6 +82,16 @@ class Auth extends Component {
     }
     this.setState({ controls: updatedControls, formIsValid: formIsValid });
   };
+
+  submitHandler = event => {
+    event.preventDefault();
+
+    // console.log(this.state.controls.email.value);
+    this.props.onAuth(
+      this.state.controls.email.value,
+      this.state.controls.password.value
+    );
+  };
   render() {
     const formElementsArray = [];
     for (let key in this.state.controls) {
@@ -89,7 +101,7 @@ class Auth extends Component {
       });
     }
     let form = (
-      <form onSubmit={this.orderHandler}>
+      <form onSubmit={this.submitHandler}>
         {formElementsArray.map(formElement => (
           <Input
             touched={formElement.config.touched}
@@ -118,4 +130,12 @@ class Auth extends Component {
   }
 }
 
-export default Auth;
+const mapDispatchToProps = dispatch => {
+  return {
+    onAuth: (email, password) => dispatch(actions.auth(email, password))
+  };
+};
+export default connect(
+  null,
+  mapDispatchToProps
+)(Auth);
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -5,3 +5,4 @@ export {
 } from "./burgerBuilder";
 
 export { purchaseBurger, purchaseInit, fetchOrders } from "./order";
+export { auth } from "./auth";
