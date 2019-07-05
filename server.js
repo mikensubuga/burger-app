eecf7d8729a96f141e455bdfad2b0433a59adf8d
King-Mike <nsubugamike021@gmail.com>
2019-07-05 13:25:17 +0300

    redirect * to index

--- server.js
+++ server.js
@@ -3,8 +3,17 @@ const serveStatic = require("serve-static");
 const path = require("path");
 // create the express app
 const app = express();
-// create middleware to handle the serving the app
-app.use("/", serveStatic(path.join(__dirname, "/build")));
+
+if (process.env.NODE_ENV === "production") {
+  // create middleware to handle the serving the app
+  app.use("/", serveStatic(path.join(__dirname, "/build")));
+
+  // Handle React routing, return all requests to React app
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "/build", "index.html"));
+  });
+}
+
 // Create default port to serve the app on
 const port = process.env.PORT || 80;
 app.listen(port);
