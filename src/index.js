459502825f1c4096c6073f18e350712563a55977
King-Mike <nsubugamike021@gmail.com>
2019-07-10 22:56:34 +0300

    using env variables

--- src/index.js
+++ src/index.js
@@ -12,7 +12,10 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers =
+  process.env.NODE_ENV === "development"
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+    : null;
 
 const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer,
